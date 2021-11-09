59e8cf153e3620f64dd5b288fc1264f1581bfe9f
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-11-08 19:38:05 -0800

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class TopCities(FlaskForm):
-    cityName = StringField('City Name',validators=[DataRequired(), Length(min=3, max=30)])
-    cityRank = IntegerField('City Rank', validators=[DataRequired()])
-    isVisited = BooleanField('Visited')
+    city_name = StringField('City Name',validators=[DataRequired(), Length(min=3, max=30)])
+    city_rank = IntegerField('City Rank', validators=[DataRequired()])
+    is_visited = BooleanField('Visited')
     submit = SubmitField('Submit')
 
