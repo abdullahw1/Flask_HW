b4c8c38c0b0c0280494c0d9a1fffb6abb3e2897e
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-11-08 19:09:30 -0800

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,10 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password')
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sign in')
-
+class TopCities(FlaskForm):
+    cityName = StringField('City Name',validators=[DataRequired(), Length(min=3, max=30)])
+    cityRank = IntegerField('City Rank', validators=[DataRequired()])
+    isVisited = BooleanField('Visited')
+    submit = SubmitField('Submit')
 
